f7da2e54ada6cfd112732677138efc2fefe9cb86
Estebanmora0910 <estebaalejandromoraavila@gmail.com>
2025-08-11 11:17:09 -0500

    Delete Controlador directory

--- Controlador/ListaClientesController.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../Modelo/ClienteModel.php';
-
-$clientes = ClienteModel::obtenerClientes();
-include __DIR__ . '/../Vista/html/lista_clientes.php';
--- Controlador/MovimientosController.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../Modelo/MovimientosModel.php';
-
-$movimientos = MovimientosModel::obtenerMovimientos();
-
-include __DIR__ . '/../Vista/html/movimientos.php';
