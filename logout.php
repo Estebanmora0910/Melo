40500bdd5d137d570ba7185670cc24922bae9372
Estebanmora0910 <estebaalejandromoraavila@gmail.com>
2025-08-11 11:20:54 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();    // Elimina todas las variables de sesión
-session_destroy();  // Destruye la sesión
-
-// Redirige al login o al inicio público
-header('Location: /melo8-main/Vista/html/login.php');
-exit;
